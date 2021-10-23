4832b4405209555ee79693f75902a7c7ee9272ed
Konrad Bogacz <konradb@poczta.onet.eu>
2021-10-23 15:57:08 +0200

    v0.1 -> Initial commit      -> Added card flip animation      -> Added card swipe animation      -> Fixed card flip bug      -> Design changes

---
+++ app/src/main/java/com/example/karcianka/MainActivity.kt
@@ -0,0 +1,128 @@
+package com.example.karcianka
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.constraintlayout.motion.widget.TransitionAdapter
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.activity_main.*
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.animation.doOnEnd
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var front_anim:AnimatorSet
+    private lateinit var back_anim:AnimatorSet
+    private var isFront = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        //Card swipe animations
+        val viewModel = ViewModelProvider(this).get(SwipeRightViewModel::class.java)
+
+        viewModel.modelStream.observe(this, Observer { bindCard(it) })
+
+        motionLayout.setTransitionListener(object: TransitionAdapter() {
+            override fun onTransitionCompleted(motionLayout: MotionLayout, currentId: Int) {
+                when (currentId) {
+                    R.id.offScreenPass,
+                    R.id.offScreenLike -> {
+                        motionLayout.progress = 0f
+                        motionLayout.setTransition(R.id.rest, R.id.like)
+                        viewModel.swipe()
+                    }
+                }
+            }
+        })
+
+
+        //Card flip animation
+        var scale = applicationContext.resources.displayMetrics.density
+
+        val front = findViewById<TextView>(R.id.card_front) as TextView
+        val back =findViewById<TextView>(R.id.card_back) as TextView
+
+        front.cameraDistance = 8000 * scale
+        back.cameraDistance = 8000 * scale
+
+        front_anim = AnimatorInflater.loadAnimator(applicationContext, R.animator.front_card_flip) as AnimatorSet
+        back_anim = AnimatorInflater.loadAnimator(applicationContext, R.animator.back_card_flip) as AnimatorSet
+
+        front.setOnClickListener{
+            if(isFront)
+            {
+                front_anim.setTarget(front);
+                back_anim.setTarget(back);
+                back.isEnabled = false
+                front.isEnabled = false
+                front_anim.start()
+                back_anim.start()
+                back_anim.doOnEnd {
+                    back.isEnabled = true
+                    front.isEnabled = true
+                }
+                isFront = false
+            }
+            else
+            {
+                front_anim.setTarget(back)
+                back_anim.setTarget(front)
+                back.isEnabled = false
+                front.isEnabled = false
+                back_anim.start()
+                front_anim.start()
+                back_anim.doOnEnd {
+                    back.isEnabled = true
+                    front.isEnabled = true
+                }
+                isFront =true
+
+            }
+        }
+
+        back.setOnClickListener{
+            if(isFront)
+            {
+                front_anim.setTarget(front);
+                back_anim.setTarget(back);
+                front.isEnabled = false
+                back.isEnabled = false
+                front_anim.start()
+                back_anim.start()
+                back_anim.doOnEnd {
+                    back.isEnabled = true
+                    front.isEnabled = true
+                }
+                isFront = false
+
+            }
+            else
+            {
+                front_anim.setTarget(back)
+                back_anim.setTarget(front)
+                front.isEnabled = false
+                back.isEnabled = false
+                back_anim.start()
+                front_anim.start()
+                back_anim.doOnEnd {
+                    back.isEnabled = true
+                    front.isEnabled = true
+                }
+                isFront =true
+
+            }
+        }
+    }
+
+    private fun bindCard(model: SwipeRightModel) {
+        topCard.setBackgroundColor(model.top.backgroundColor)
+        bottomCard.setBackgroundColor(model.bottom.backgroundColor)
+    }
+}
---
+++ app/src/main/java/com/example/karcianka/SwipeRightViewModel.kt
@@ -0,0 +1,43 @@
+package com.example.karcianka
+
+import android.graphics.Color
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.MutableLiveData
+
+class SwipeRightViewModel:ViewModel() {
+
+    private val stream = MutableLiveData<SwipeRightModel>()
+
+    val modelStream: LiveData<SwipeRightModel>
+        get() = stream
+
+    private val data = listOf(
+        SwipeRightCardModel(backgroundColor = Color.parseColor("#FFFFFF")),
+        SwipeRightCardModel(backgroundColor = Color.parseColor("#FFFFFF")),
+        SwipeRightCardModel(backgroundColor = Color.parseColor("#FFFFFF")),
+        SwipeRightCardModel(backgroundColor = Color.parseColor("#FFFFFF"))
+    )
+    private var currentIndex = 0
+
+    private val topCard
+        get() = data[currentIndex % data.size]
+    private val bottomCard
+        get() = data[(currentIndex + 1) % data.size]
+
+    init {
+        updateStream()
+    }
+
+    fun swipe() {
+        currentIndex += 1
+        updateStream()
+    }
+
+    private fun updateStream() {
+        stream.value = SwipeRightModel(
+            top = topCard,
+            bottom = bottomCard
+        )
+    }
+}
